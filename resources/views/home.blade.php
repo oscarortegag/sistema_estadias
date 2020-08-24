939421f480be234bf966d63a86fdeda6b8cc791b
Sistemas Universidad Tecnológica de Chetumal <william.sanchez@utchetumal.edu.mx>
2020-08-24 06:50:24 -0500

    Validar acceso por middleware y roles

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -53,32 +53,6 @@
 
 @section('content')
     <h1> Bienvenido al sistema de seguimiento a egresados 2020</h1>
-    <p class="text-primary">.text-primary</p>
-    <p class="text-secondary">.text-secondary</p>
-    <p class="text-success">.text-success</p>
-    <p class="text-danger">.text-danger</p>
-    <p class="text-warning">.text-warning</p>
-    <p class="text-info">.text-info</p>
-    <p class="text-light bg-dark">.text-light</p>
-    <p class="text-dark">.text-dark</p>
-    <p class="text-muted">.text-muted</p>
-    <p class="text-white bg-dark">.text-white</p>
-
-    <ul class="fc-color-picker" id="color-chooser">
-        <li><a class="text-aqua" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-blue" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-light-blue" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-teal" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-yellow" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-orange" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-green" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-lime" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-red" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-purple" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-fuchsia" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-muted" href="#"><i class="fa fa-square"></i></a></li>
-        <li><a class="text-navy" href="#"><i class="fa fa-square"></i></a></li>
-    </ul>
 
     <select>
         <option class="text-aqua" style="background-color: #f012be">
