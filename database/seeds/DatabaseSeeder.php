33dd9530146ecf2fc3fbee3197feb0d6cb30b2cc
Sistemas Universidad Tecnológica de Chetumal <william.sanchez@utchetumal.edu.mx>
2020-08-25 19:42:55 -0500

    Agregar seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        // La creación de datos de roles debe ejecutarse primero
+        $this->call(RoleSeeder::class);
+        // Los usuarios necesitarán los roles previamente generados
+        $this->call(UserSeeder::class);
 
-       $this->call(UserTableSeeder::class);
+        $files = [
+            'database/queries/colors.sql',
+            'database/queries/states.sql',
+        ];
 
+        foreach ($files as $path) {
+            DB::unprepared(file_get_contents($path));
+            $this->command->info("{$path} has been run");
+        }
     }
 }
